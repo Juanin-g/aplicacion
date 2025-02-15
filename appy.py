c0e70fd6afba89a94d14221183ebfdfaa245dd54
Juanin-g <jg9050326@gmail.com>
2025-02-15 13:41:09 -0600

    Update appy.py

--- appy.py
+++ appy.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import pyplot
+import matplotlib.pyplot as pyplot
 #leer datos
 titanic = pd.read_csv("titanic.csv")
  # Crear un gráfico con Plotly (por ejemplo, un gráfico de barras de la edad)
