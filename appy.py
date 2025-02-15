bb5edd321b8dda4b174c9af986fb13f09c8291d7
Juanin-g <jg9050326@gmail.com>
2025-02-15 13:18:01 -0600

    Create appy.py

---
+++ appy.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+#leer datos
+titanic = pd.read_csv("titanic.csv")
+
+# Configuración de la página
+st.set_page_config(page_title="Dashboars", layout="wide")
+st.sidebar.title("Analisis de datos")
+
+# Pestañas
+tab1, tab2 = st.tabs(["Cuadros de desglose", "Graficas"])
+with tab1
+  st.header("informaciòn general")
+  st.plotly_chart(titanic.head())
+  st.plotly_chart(titanic.info())
+
+with tab2
+  st.plotly_chart(titanic.plot(subplots= True, figsize=(15, 12), sharex= False, sharey=False))
+
